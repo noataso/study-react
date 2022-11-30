6fac5719d00be1c87e44cd1917d93a83706e4db1
noataso <noa740740@outlook.jp>
2022-12-01 03:31:44 +0900

    ♻️ ヘッドライン部分をコンポーネント化

--- components/Links.js
+++ components/Links.js
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {Footer} from "./Footer"
 
 export function Links() {
   return (
